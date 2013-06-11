16f9361938610efc8cfa609abb532984b2e21e70
acheung456 <acheung456@gmail.com>
2013-06-11 15:05:33 -0400

    Adding local project files to repo.

---
+++ Jun13/Jun13AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun13AppDelegate.h
+//  Jun13
+//
+//  Created by tslocal on 6/11/13.
+//  Copyright (c) 2013 acheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun13AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
